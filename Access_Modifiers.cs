79e3a51e522cb414be40fb41aca1b50e52a88997
ChandarAwesome <ajaygames00Gmail.com>
2023-03-18 12:49:16 +0530

    [Chandra]

---
+++ Access_Modifiers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+     class Access_Modifiers
+    {
+        private int x = 10;
+        public int y = 20;
+        protected int z = 30;
+
+        public static  void Methodss()
+        {
+            Access_Modifiers access= new Access_Modifiers(); // to access the private field
+            Console.WriteLine(access.x);
+            Console.WriteLine(access.z);
+        }
+    } 
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+            //Access_Modifiers access= new Access_Modifiers();
+            //Console.WriteLine(access.y); // access the public modifier data
+            //Console.WriteLine(access.z);// we got error coz of z is a protected data type
+            //Console.WriteLine(access.x);// x is an private data type [error]
+            //
              Console.WriteLine("Hello, World!");
             //Triangle.ReverseAngleTriangle();
             //Triangle.RightAngleTriangle();
